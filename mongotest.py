a1c31c4360139c009c2bd03256e20dd627d3258a
knave <knaveenkumar205@gmail.com>
2022-07-20 17:26:34 +0530

    my first code push

---
+++ mongotest.py
@@ -0,0 +1,9 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d={
+    "name":"sudhanshu",
+    "email":"sudh"
+}
